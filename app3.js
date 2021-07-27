bf91c72c69b755d8543cae59b9c105e791bd8326
LuciLua <luci.lua81@gmail.com>
2021-07-27 16:13:05 -0300

    :computer: drag test2

--- app3.js
+++ app3.js
@@ -1,13 +1,50 @@
-function handleDragStart(e) {
-    this.style.opacity = '0.4';
-  }
+document.addEventListener('DOMContentLoaded', (event) => {
 
-  function handleDragEnd(e) {
-    this.style.opacity = '1';
-  }
-
-  let items = document.querySelectorAll('.container .box');
-  items.forEach(function(item) {
-    item.addEventListener('dragstart', handleDragStart, false);
-    item.addEventListener('dragend', handleDragEnd, false);
+    function handleDragStart(e) {
+        this.style.opacity = '0.4';
+      
+        dragSrcEl = this;
+      
+        // e.dataTransfer.effectAllowed = 'move';
+        // e.dataTransfer.setData('text/html', this.innerHTML);
+      }
+  
+    function handleDragEnd(e) {
+      this.style.opacity = '0.1';
+  
+      items.forEach(function (item) {
+        item.classList.remove('over');
+      });
+    }
+  
+    function handleDragOver(e) {
+      if (e.preventDefault) {
+        e.preventDefault();
+      }
+  
+      return false;
+    }
+  
+    function handleDragEnter(e) {
+      this.classList.add('over');
+    }
+  
+    function handleDragLeave(e) {
+      this.classList.remove('over');
+    }
+  
+    let items = document.querySelectorAll('.box .par');
+    items.forEach(function(item) {
+      item.addEventListener('dragstart', handleDragStart, false);
+      item.addEventListener('dragover', handleDragOver, false);
+      item.addEventListener('dragenter', handleDragEnter, false);
+      item.addEventListener('dragleave', handleDragLeave, false);
+      item.addEventListener('dragend', handleDragEnd, false);
+    });
   });
+
+
+  function handleDrop(e) {
+    e.stopPropagation(); // stops the browser from redirecting.
+    return false;
+  }
