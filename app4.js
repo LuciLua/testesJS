6788c53b06edf3aa9bc4e3f7d48b50745bf6d052
LuciLua <luci.lua81@gmail.com>
2021-07-31 15:00:40 -0300

    :computer: SetTimeInterval: time for press button

--- app4.js
+++ app4.js
@@ -1,33 +1,32 @@
 window.onload = () => {
 var keys = document.getElementById('keys')
 var random = document.getElementById('random')
-var result = document.getElementById('result')
+var ram = ram
 
-document.querySelector('body').addEventListener('keydown', function(e){
-
+setInterval( function(){
     let opcoes = ['q','w','e','r']
-
-    for (let i = 0; i < opcoes.length; i++){
-        console.log(i+1, opcoes[i])
-        console.log((Math.random() * 10).toFixed())
-    }
-
+    
     let shuffled = opcoes
     .map( (value) => ({ value, sort: Math.random() }) )
     .sort( (a, b) => a.sort - b.sort)
     .map( ({ value }) => value)
 
-    random.textContent = shuffled[0] //computer
+    random.textContent = shuffled[0]
+    ram =  shuffled[0]
+    } , 900)
 
+document.querySelector('body').addEventListener('keydown', function(e){
     keys.textContent=(e.key) //user 
 
-    if (e.key == shuffled[0]){
+    var result = document.getElementById('result')
+
+    if (e.key == ram){
         result.innerHTML='❤'
     }
     else {
         result.innerHTML='☠'
     }
 
-    })
-}
 
+})
+}
