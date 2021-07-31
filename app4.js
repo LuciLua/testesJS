797e685caf42738d18514b718907dfb642154e74
LuciLua <luci.lua81@gmail.com>
2021-07-31 16:07:49 -0300

    :computer: Organized | time defined

--- app4.js
+++ app4.js
@@ -1,10 +1,30 @@
 window.onload = () => {
+
+document.querySelector('body').addEventListener('keydown', function(e){ //ouve press
+keys.textContent=(e.key) //user 
+keyPress = e.key //user press...
+})
+
 var keys = document.getElementById('keys')
 var random = document.getElementById('random')
+
+var userPts = document.getElementById('userPts')
+var compPts = document.getElementById('compPts')
+
+var result = document.getElementById('result')
+
 var ram = ram
 var keyPress = keyPress
 
-setInterval( function(){
+var userWin = 0
+var compWin = 0
+
+var user = 0 
+var comp = 0
+
+var time = 500
+
+setInterval( function(){ // sorteia letra
     let opcoes = ['q', 'w', 'e', 'r']
     
     let shuffled = opcoes
@@ -14,20 +34,9 @@ setInterval( function(){
 
     random.textContent = shuffled[0]
     ram =  shuffled[0]
-    }, 900)
+    }, time)
 
-
-document.querySelector('body').addEventListener('keydown', function(e){
-    keys.textContent=(e.key) //user 
-    keyPress = e.key //user press...
-})
-
-var userWin = 0
-var compWin = 0
-
-var result = document.getElementById('result')
-setInterval( function(){ //verifica de quanto em quanto tempo
-
+setInterval( function(){ // ve se a digitada é a pedida
     if (keyPress == ram){
         result.innerHTML='❤'
         userWin = 1
@@ -39,30 +48,18 @@ setInterval( function(){ //verifica de quanto em quanto tempo
         compWin = 1
     }
 
-    }, 500)
+}, time)
 
 
-    var user = 0 
-    var comp = 0 
-    var userPts = document.getElementById('userPts')
-    var compPts = document.getElementById('compPts')
-
-    setInterval( function(){
-        if(userWin == 1){
+setInterval( function(){ // velocidade do placar
+    if(userWin == 1){
         user++
-        console.log(user)
-    
         userPts.textContent = user
-        compPts.textContent = comp
-        }
-    }, 500)
-
-    setInterval( function(){
-        if(compWin == 1){
+    }
+    if(compWin == 1){
         comp++
-
-        userPts.textContent = user
         compPts.textContent = comp
-        }
-    }, 500)
+    }
+}, 100)
+
 }
